a07891c9f4013112dde2fc1e47c3a242e06807e9
cock45 <stardevworld@gmail.com>
2022-10-25 04:13:00 +0900

    Update: "Use const type instead of var type"

--- data-structure/arrays-ds.js
+++ data-structure/arrays-ds.js
@@ -1,6 +1,6 @@
 function reverseArray(a) {
   // Write your code here
-  var reversedArray = [];
+  const reversedArray = [];
   for(var i = a.length - 1; i >= 0; i--) {
      reversedArray.push(a[i]);
   };
