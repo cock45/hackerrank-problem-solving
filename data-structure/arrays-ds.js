6260173a35ca678d65843d9691c7f2c02b3cf55d
cock45 <stardevworld@gmail.com>
2022-10-25 04:27:03 +0900

    Update: "Use let type instead of var type"

--- data-structure/arrays-ds.js
+++ data-structure/arrays-ds.js
@@ -1,7 +1,7 @@
 function reverseArray(a) {
   // Write your code here
   const reversedArray = [];
-  for(var i = a.length - 1; i >= 0; i--) {
+  for(let i = a.length - 1; i >= 0; i--) {
      reversedArray.push(a[i]);
   };
   return reversedArray;
