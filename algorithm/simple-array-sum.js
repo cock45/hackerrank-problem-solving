b5aa09a07ab742703958c4eea7c8e0f2fdb7c00d
cock45 <stardevworld@gmail.com>
2022-10-25 01:34:23 +0900

    Update: "Save lines of code by using arrow function"

--- algorithm/simple-array-sum.js
+++ algorithm/simple-array-sum.js
@@ -1,7 +1,5 @@
 function simpleArraySum(ar) {
   // Write your code here
      
-  return ar.reduce((total, value) => {
-      return total + value;
-  }, 0);
+  return ar.reduce((total, value) => total + value, 0);
 }
