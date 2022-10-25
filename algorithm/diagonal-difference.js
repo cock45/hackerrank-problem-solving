1e5e7763b5fca93ea943a81071c7241d616c4d1a
cock45 <stardevworld@gmail.com>
2022-10-26 03:25:30 +0900

    Feat: "Diagonal Difference"

---
+++ algorithm/diagonal-difference.js
@@ -0,0 +1,12 @@
+function diagonalDifference(arr) {
+  // Write your code here
+  let leftToRight = 0;
+  let rightToLeft = 0;
+  
+  for(let i = 0; i < arr.length; i++) {
+      leftToRight += arr[i][i];
+      rightToLeft += arr[i][arr.length - i - 1];
+  }
+  
+  return Math.abs(leftToRight - rightToLeft);
+}
