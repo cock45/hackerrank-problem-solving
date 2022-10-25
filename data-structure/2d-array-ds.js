9392541bfb407c484eb3aa76d3acb73fcc523acc
cock45 <stardevworld@gmail.com>
2022-10-26 03:53:51 +0900

    Update: "Use const for sum variable"

--- data-structure/2d-array-ds.js
+++ data-structure/2d-array-ds.js
@@ -1,11 +1,11 @@
 function hourglassSum(arr) {
   // Write your code here
-  let max = -63;
+  let max = -9 * 7;
   
   for (let i = 1; i < 5; i++) 
-      for ( let j = 1; j < 5; j++) {
-      let sum = arr[i - 1][j - 1] + arr[i - 1][j] + arr[i - 1][j + 1] + arr[i][j] + arr[i + 1][j - 1] + arr[i + 1][j] + arr[i + 1][j + 1];
-      max = sum > max ? sum : max;
+      for (let j = 1; j < 5; j++) {
+        const sum = arr[i - 1][j - 1] + arr[i - 1][j] + arr[i - 1][j + 1] + arr[i][j] + arr[i + 1][j - 1] + arr[i + 1][j] + arr[i + 1][j + 1];
+        max = sum > max ? sum : max;
       }
   
   return max;
