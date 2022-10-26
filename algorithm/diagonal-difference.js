e67a83de8f820d0001c7a5ba47bfa878f1873eba
cock45 <stardevworld@gmail.com>
2022-10-26 21:52:10 +0900

    Update: "#23 Use += instead of ="

--- algorithm/diagonal-difference.js
+++ algorithm/diagonal-difference.js
@@ -1,10 +1,10 @@
 function diagonalDifference(arr) {
   // Write your code here
   let result = 0;
-  
-  for(let i = 0; i < arr.length; i++) {
-      result = result + arr[i][i] - arr[i][arr.length - i - 1];
+
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i][i] - arr[i][arr.length - i - 1];
   }
-  
+
   return Math.abs(result);
 }
