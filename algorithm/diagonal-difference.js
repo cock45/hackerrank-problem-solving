6bdb7fdc354141ac210c1f1a02b06b1ca12a1937
cock45 <stardevworld@gmail.com>
2022-10-26 21:02:41 +0900

    Update: "#23 Works with one variable instead of using two."

--- algorithm/diagonal-difference.js
+++ algorithm/diagonal-difference.js
@@ -1,12 +1,10 @@
 function diagonalDifference(arr) {
   // Write your code here
-  let leftToRight = 0;
-  let rightToLeft = 0;
+  let result = 0;
   
   for(let i = 0; i < arr.length; i++) {
-      leftToRight += arr[i][i];
-      rightToLeft += arr[i][arr.length - i - 1];
+      result = result + arr[i][i] - arr[i][arr.length - i - 1];
   }
   
-  return Math.abs(leftToRight - rightToLeft);
+  return Math.abs(result);
 }
