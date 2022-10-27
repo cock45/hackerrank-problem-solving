717a0b03487e442fd9c34fb3481cff877627574d
cock45 <stardevworld@gmail.com>
2022-10-27 22:15:54 +0900

    Update: "#37 Modify code as more readable"

--- algorithm/between-two-sets.js
+++ algorithm/between-two-sets.js
@@ -1,24 +1,21 @@
 function getTotalX(a, b) {
   // Write your code here
-  let total = 0;
-
-  const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+  const gcd = (a, b) => (!b ? a : gcd(b, a % b));
   const lcm = (a, b) => (a * b) / gcd(a, b);
-  const countOfFactors = (value) => {
-    let count = 0;
-    if (value === 1) return count;
-    for (let i = 2; i <= value / 2; i++) {
-      if (value % i === 0) {
-        count++;
-      }
+  const lcmA = a.reduce((prev, cur) => lcm(prev, cur));
+  const gcdB = b.reduce((prev, cur) => gcd(prev, cur));
+
+  if (gcdB % lcmA !== 0) {
+    return 0;
+  }
+
+  const num = gcdB / lcmA;
+  let count = 1;
+  for (let i = 1; 2 * i <= num; i++) {
+    if (num % i === 0) {
+      count++;
     }
-    return count + 1;
-  };
+  }
 
-  const lcmFactor = a.reduce((a, b) => lcm(a, b));
-  const gcdFactor = b.reduce((a, b) => gcd(a, b));
-
-  return gcdFactor % lcmFactor === 0
-    ? countOfFactors(gcdFactor / lcmFactor) + 1
-    : 0;
+  return count;
 }
