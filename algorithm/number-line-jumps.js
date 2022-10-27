e9148aebd88840a842cbfbd512cb185309038e8a
cock45 <stardevworld@gmail.com>
2022-10-27 10:01:04 +0900

    Update: "#35 Use tenary instead of if-else"

--- algorithm/number-line-jumps.js
+++ algorithm/number-line-jumps.js
@@ -1,11 +1,7 @@
 function kangaroo(x1, v1, x2, v2) {
   // Write your code here
-  const dist = x1 - x2;
-  const speed = v1 - v2;
+  const dx = x1 - x2;
+  const dv = v1 - v2;
 
-  if (dist / speed < 0 && dist % speed === 0) {
-    return "YES";
-  } else {
-    return "NO";
-  }
+  return dx / dv < 0 && dx % dv === 0 ? "YES" : "NO";
 }
