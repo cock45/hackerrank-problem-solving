ee53fbd4462d16baad996203148754dc50fae243
cock45 <stardevworld@gmail.com>
2022-10-28 23:03:00 +0900

    Feat: "Counting Valleys"

---
+++ algorithm/counting-valleys.js
@@ -0,0 +1,19 @@
+function countingValleys(steps, path) {
+  // Write your code here
+  let number = 0
+  let position = 0
+
+  for (let i = 0; i < steps; i++) {
+    if (path.charAt(i) === 'U') {
+      position++
+
+      if (position === 0) {
+        number++
+      }
+    } else {
+      position--
+    }
+  }
+
+  return number
+}
