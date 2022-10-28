c087d3bf9beee393d24f291fc38d0dce189cb4c0
cock45 <stardevworld@gmail.com>
2022-10-29 05:45:35 +0900

    Update: "#53 Add semicolon at the end of the line"

--- algorithm/cats-and-a-mouse.js
+++ algorithm/cats-and-a-mouse.js
@@ -1,5 +1,5 @@
 function catAndMouse(x, y, z) {
-  const result = ['Cat A', 'Mouse C', 'Cat B']
+    const result = ['Cat A', 'Mouse C', 'Cat B'];
 
-  return result[Math.sign(Math.abs(x - z) - Math.abs(y - z)) + 1]
+    return result[Math.sign(Math.abs(x - z) - Math.abs(y - z)) + 1];
 }
