499aa9bfea9e1983063ac93920c713fee892ba1d
cock45 <stardevworld@gmail.com>
2022-10-28 14:52:27 +0900

    Update: "#44 Recommit"

---
+++ algorithm/bill-division.js
@@ -0,0 +1,6 @@
+function bonAppetit(bill, k, b) {
+  // Write your code here
+  const anna = (bill.reduce((total, price) => total + price, 0) - bill[k]) / 2;
+
+  console.log(b === anna ? "Bon Appetit" : b - anna);
+}
