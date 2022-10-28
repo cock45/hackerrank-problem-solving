995f9e3e72aa1abe9ccb632378f4a38359d8fe3a
cock45 <stardevworld@gmail.com>
2022-10-28 14:09:11 +0900

    Update: "#42 Works with comma operator"

--- algorithm/migratory-birds.js
+++ algorithm/migratory-birds.js
@@ -1,8 +1,7 @@
 function migratoryBirds(arr) {
   // Write your code here
   const frequencies = arr.reduce((freq, bird) => {
-    ++freq[bird - 1];
-    return freq;
+    return ++freq[bird - 1], freq;
   }, Array(5).fill(0));
 
   return frequencies.indexOf(Math.max(...frequencies)) + 1;
