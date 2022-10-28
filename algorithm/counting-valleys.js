59e257977d86ae2daa071c36f35235ea0623b45d
cock45 <stardevworld@gmail.com>
2022-10-29 05:48:34 +0900

    Update: "#50 Add semicolon at the end of the line"

--- algorithm/counting-valleys.js
+++ algorithm/counting-valleys.js
@@ -1,19 +1,19 @@
 function countingValleys(steps, path) {
-  // Write your code here
-  let number = 0
-  let position = 0
+    // Write your code here
+    let number = 0;
+    let position = 0;
 
-  for (let i = 0; i < steps; i++) {
-    if (path.charAt(i) === 'U') {
-      position++
+    for (let i = 0; i < steps; i++) {
+        if (path.charAt(i) === 'U') {
+            position++;
 
-      if (position === 0) {
-        number++
-      }
-    } else {
-      position--
+            if (position === 0) {
+                number++;
+            }
+        } else {
+            position--;
+        }
     }
-  }
 
-  return number
+    return number;
 }
