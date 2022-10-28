60ec8f49b0f855c7a74f2b59b56ae5a90811c29f
cock45 <stardevworld@gmail.com>
2022-10-28 23:52:27 +0900

    Feat: "Electronics Shop"

---
+++ algorithm/electronics-shop.js
@@ -0,0 +1,18 @@
+function getMoneySpent(keyboards, drives, b) {
+  /*
+   * Write your code here.
+   */
+  let maxPrice = -1
+
+  for (let k = 0; k < keyboards.length; k++) {
+    for (let d = 0; d < drives.length; d++) {
+      const price = keyboards[k] + drives[d]
+
+      if (price > maxPrice && price <= b) {
+        maxPrice = price
+      }
+    }
+  }
+
+  return maxPrice
+}
