0debb9da47bacc89eb4decd8454d53c06ec6a802
cock45 <stardevworld@gmail.com>
2022-10-28 15:08:26 +0900

    Update: "Return result without using return"

--- algorithm/day-of-the-programer.js
+++ algorithm/day-of-the-programer.js
@@ -2,11 +2,7 @@ function dayOfProgrammer(year) {
   // Write your code here
   const daysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-  const isLeap = (year) => {
-    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
-      ? true
-      : false;
-  };
+  const isLeap = (yy) => (yy % 4 === 0 && yy % 100 !== 0) || yy % 400 === 0;
 
   if ((year < 1918 && year % 4 === 0) || (year >= 1918 && isLeap(year))) {
     daysOfMonths[1] = 29;
