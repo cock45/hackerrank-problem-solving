755245c2e942ad33a33274af5f1a8e721de8711f
cock45 <stardevworld@gmail.com>
2022-10-28 14:31:23 +0900

    Update: "#43 Modify isLeap as more usable"

--- algorithm/day-of-the-programer.js
+++ algorithm/day-of-the-programer.js
@@ -3,14 +3,12 @@ function dayOfProgrammer(year) {
   const daysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
   const isLeap = (year) => {
-    return (year <= 1918 && year % 4 === 0) ||
-      (year > 1918 && year % 4 === 0 && year % 100 !== 0) ||
-      year % 400 === 0
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
       ? true
       : false;
   };
 
-  if (isLeap(year)) {
+  if ((year < 1918 && year % 4 === 0) || (year >= 1918 && isLeap(year))) {
     daysOfMonths[1] = 29;
   }
 
