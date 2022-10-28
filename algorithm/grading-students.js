756b8804fa211303e3e6e95fb82a10ef56b71957
cock45 <stardevworld@gmail.com>
2022-10-29 05:47:45 +0900

    Update: "#51 Add semicolon at the end of the line"

--- algorithm/grading-students.js
+++ algorithm/grading-students.js
@@ -1,10 +1,10 @@
 function gradingStudents(grades) {
-  // Write your code here
-  return grades.map((grade) => {
-    if (grade >= 38 && grade % 5 >= 3) {
-      return 5 * Math.ceil(grade / 5)
-    }
+    // Write your code here
+    return grades.map((grade) => {
+        if (grade >= 38 && grade % 5 >= 3) {
+            return 5 * Math.ceil(grade / 5);
+        }
 
-    return grade
-  })
+        return grade;
+    });
 }
