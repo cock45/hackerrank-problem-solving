8b25a77b305bba8b5bbec48fa11cefdab102580b
cock45 <stardevworld@gmail.com>
2022-10-31 21:35:50 +0900

    Update: "#53 Use if instead of else if"

--- algorithm/cats-and-a-mouse.js
+++ algorithm/cats-and-a-mouse.js
@@ -1,13 +1,14 @@
 function catAndMouse(x, y, z) {
-    const result = ['Cat A', 'Mouse C', 'Cat B'];
     const fromAtoC = Math.abs(x - z);
     const fromBtoC = Math.abs(y - z);
 
     if (fromAtoC > fromBtoC) {
         return 'Cat A';
-    } else if (fromAtoC < fromBtoC) {
+    } 
+    
+    if (fromAtoC < fromBtoC) {
         return 'Cat B';
-    } else {
-        return 'Mouse C';
-    }
+    } 
+
+    return 'Mouse C';
 }
