fb85ac52891764d666378042f493e909214ce140
cock45 <stardevworld@gmail.com>
2022-10-31 21:43:14 +0900

    Update: "#48 Use Math.floor() instead of parseInt"

--- algorithm/sales-by-match.js
+++ algorithm/sales-by-match.js
@@ -2,5 +2,5 @@ function sockMerchant(n, ar) {
     // Write your code here
     const frequences = ar.reduce((freq, value) => (++freq[value - 1], freq), new Array(100).fill(0));
 
-    return frequences.reduce((count, value) => count += parseInt(value / 2), 0);
+    return frequences.reduce((count, value) => count + Math.floor(value / 2), 0);
 }
