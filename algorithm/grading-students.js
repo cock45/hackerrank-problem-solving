6242090a41bc5b72a1b9a2fdb08e270f8fde0311
cock45 <stardevworld@gmail.com>
2022-10-31 17:51:19 +0900

    Update: "#51 Works without Math.ceil"

--- algorithm/grading-students.js
+++ algorithm/grading-students.js
@@ -2,7 +2,7 @@ function gradingStudents(grades) {
     // Write your code here
     return grades.map((grade) => {
         if (grade >= 38 && grade % 5 >= 3) {
-            return 5 * Math.ceil(grade / 5);
+            return grade - grade % 5 + 5;
         }
 
         return grade;
