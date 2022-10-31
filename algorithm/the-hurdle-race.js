fde31ba6a124909c060517f8db715c32b1263243
cock45 <stardevworld@gmail.com>
2022-11-01 06:49:55 +0900

    Update: "Replace tenary into Math.max()"

--- algorithm/the-hurdle-race.js
+++ algorithm/the-hurdle-race.js
@@ -1,6 +1,4 @@
 function hurdleRace(k, height) {
     // Write your code here
-    const doses = Math.max(...height) - k;
-
-    return doses > 0 ? doses : 0;
+    return Math.max(Math.max(...height) - k, 0);
 }
