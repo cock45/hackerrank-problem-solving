833389ebe6323ef9eff9fe7e4fd6212e36bb4ae5
cock45 <stardevworld@gmail.com>
2022-11-01 11:39:27 +0900

    Update: "#59 Use bitwise shift insteadof Math.pow"

--- algorithm/utopian-tree.js
+++ algorithm/utopian-tree.js
@@ -1,4 +1,4 @@
 function utopianTree(n) {
     // Write your code here
-    return Math.pow(2, Math.ceil(n / 2) + 1) - Math.pow(2, n % 2);
+    return (1 << (Math.ceil(n / 2) + 1)) - (1 << n % 2);
 }
