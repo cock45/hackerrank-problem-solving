625956c812cc9c0a248b37c8b9de40433381c00a
cock45 <stardevworld@gmail.com>
2022-11-01 22:33:52 +0900

    Update: "Solve TIme limit error"

--- algorithm/climbing-the-leaderboard.js
+++ algorithm/climbing-the-leaderboard.js
@@ -1,30 +1,28 @@
 function climbingLeaderboard(ranked, player) {
     // Write your code here
-    const scores = [...ranked, ...player].sort((prev, cur) => cur - prev);
-
-    let prev = 0;
     const fixedRank = [];
 
-    for (let score of scores) {
-        if (score !== prev) {
-            prev = score;
-            fixedRank.push(score);
+    for (let cur in ranked) {
+        if (ranked[cur] !== ranked[cur - 1]) {
+            fixedRank.push(ranked[cur]);
         }
     }
 
-    let prevScore = player[player.length - 1];
-    let count = 0;
+    let pos = fixedRank.length;
 
-    const ranking = player.reverse().map((value) => {
-        if (value !== prevScore && ranked.indexOf(prevScore) === -1) {
-            count++;
+    const ranking = player.map((value) => {
+        for (let i = pos - 1; i >= 0; i--) {
+            if (fixedRank[i] > value) {
+                pos = i + 1;
+                return pos + 1;
+            }
+
+            if (value >= fixedRank[0]) {
+                pos = 1;
+                return pos;
+            }
         }
-        const rank = fixedRank.indexOf(value) + 1 - count;
-
-        prevScore = value;
-
-        return rank;
     });
 
-    return ranking.reverse();
+    return ranking;
 }
