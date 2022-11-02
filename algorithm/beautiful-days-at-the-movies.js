a6444f3a04047b769d0e58e96bca87e16018e52e
cock45 <stardevworld@gmail.com>
2022-11-03 04:50:14 +0900

    Update: "Without string manipulation"

--- algorithm/beautiful-days-at-the-movies.js
+++ algorithm/beautiful-days-at-the-movies.js
@@ -1,11 +1,18 @@
+// O(n * m)
+// n: j - i, m: j.toString().length
+
 function beautifulDays(i, j, k) {
     // Write your code here
     let count = 0;
 
     for (let d = i; d <= j; d++) {
-        const reverseD = d.toString().split('').reverse().join('');
+        let rev = 0;
 
-        if (Math.abs(d - reverseD) % k === 0) {
+        for (let r = 0; r < d.toString().length; r++) {
+            rev += d.toString()[r] * Math.pow(10, r);
+        }
+
+        if (Math.abs(d - rev) % k === 0) {
             count++;
         }
     }
