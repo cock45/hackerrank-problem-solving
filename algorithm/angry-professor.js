0cd71eb30c5a7152283eea34bf8aadf0ca747c49
cock45 <stardevworld@gmail.com>
2022-11-02 14:03:56 +0900

    Update: "#60 Optimized code"

--- algorithm/angry-professor.js
+++ algorithm/angry-professor.js
@@ -1,4 +1,4 @@
 function angryProfessor(k, a) {
     // Write your code here
-    return a.filter((value) => value > 0).length > a.length - k ? 'YES' : 'NO';
+    return a.filter((value) => value <= 0).length < k ? 'YES' : 'NO';
 }
