04ff42c2af6793a6c799cd96c988f66f8bf82489
cock45 <stardevworld@gmail.com>
2022-11-03 22:21:54 +0900

    Update: "Saved 4 Code lines"

--- algorithm/jumping-on-clouds.js
+++ algorithm/jumping-on-clouds.js
@@ -5,12 +5,7 @@ function jumpingOnClouds(c, k) {
     let cloud = 0;
 
     do {
-        cloud += k;
-
-        if (cloud >= c.length) {
-            cloud %= c.length;
-        }
-
+        cloud = (cloud + k) % c.length;
         energy -= 2 * c[cloud] + 1;
     } while (cloud);
 
