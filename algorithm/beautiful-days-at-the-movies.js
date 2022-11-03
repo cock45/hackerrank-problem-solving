4ce2ed8b454e5b346349f71336677080c0d64d7d
cock45 <stardevworld@gmail.com>
2022-11-03 09:57:49 +0900

    Update: "#61 By defining Number reverse()"

--- algorithm/beautiful-days-at-the-movies.js
+++ algorithm/beautiful-days-at-the-movies.js
@@ -3,16 +3,21 @@
 
 function beautifulDays(i, j, k) {
     // Write your code here
-    let count = 0;
-
-    for (let d = i; d <= j; d++) {
+    const reverse = (num) => {
+        let temp = num;
         let rev = 0;
 
-        for (let r = 0; r < d.toString().length; r++) {
-            rev += d.toString()[r] * Math.pow(10, r);
+        while (temp) {
+            rev = rev * 10 + (temp % 10);
+            temp = Math.floor(temp / 10);
         }
 
-        if (Math.abs(d - rev) % k === 0) {
+        return rev;
+    };
+
+    let count = 0;
+    for (let d = i; d <= j; d++) {
+        if (Math.abs(d - reverse(d)) % k === 0) {
             count++;
         }
     }
