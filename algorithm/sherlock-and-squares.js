d4d902958ef7e4996f3c40c0c72a55ce2287142a
cock45 <stardevworld@gmail.com>
2022-11-04 01:28:43 +0900

    Update: "#70 Works without for statement"

--- algorithm/sherlock-and-squares.js
+++ algorithm/sherlock-and-squares.js
@@ -1,12 +1,6 @@
-// O(n); n: Math.floor(Math.sqrt(b)) - Math.ceil(Math.sqrt(a))
+// O(1)
 
 function squares(a, b) {
     // Write your code here
-    let count = 0;
-
-    for (let i = Math.ceil(Math.sqrt(a)); i * i <= b; i++) {
-        count++;
-    }
-
-    return count;
+    return Math.floor(Math.sqrt(b)) - Math.ceil(Math.sqrt(a)) + 1;
 }
