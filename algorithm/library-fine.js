49d950faa09738aed466fef60e984e9dbccaecf0
cock45 <stardevworld@gmail.com>
2022-11-04 10:29:47 +0900

    Update: "#72 Without Using Logical Operator"

--- algorithm/library-fine.js
+++ algorithm/library-fine.js
@@ -2,17 +2,17 @@
 
 function libraryFine(d1, m1, y1, d2, m2, y2) {
     // Write your code here
-    if (
-        y1 < y2 ||
-        (y1 === y2 && m1 < m2) ||
-        (y1 === y2 && m1 === m2 && d1 <= d2)
-    ) {
+    if (y1 < y2) {
         return 0;
-    } else if (y1 === y2 && m1 === m2 && d1 > d2) {
-        return 15 * (d1 - d2);
-    } else if (y1 === y2 && m1 > m2) {
-        return 500 * (m1 - m2);
-    } else {
+    } else if (y1 > y2) {
         return 10000;
+    } else if (m1 < m2) {
+        return 0;
+    } else if (m1 > m2) {
+        return 500 * (m1 - m2);
     }
+    if (d1 > d2) {
+        return 15 * (d1 - d2);
+    }
+    return 0;
 }
