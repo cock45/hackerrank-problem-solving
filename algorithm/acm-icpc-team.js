86040433dc9b78a91faf506ba93f498954142dd7
cock45 <stardevworld@gmail.com>
2022-11-08 04:18:26 +0900

    Feat: "ACM ICPC Team"

---
+++ algorithm/acm-icpc-team.js
@@ -0,0 +1,29 @@
+// O (|topic|! * n), n is Number of Topics
+
+function acmTeam(topic) {
+    // Write your code here
+    let maxNum = 0;
+    let bestTeams = 0;
+
+    for (let i = 0; i < topic.length; i++) {
+        for (let j = i + 1; j < topic.length; j++) {
+            let canBeSolved = topic[i]
+                .split('')
+                .reduce(
+                    (total, value, index) => total + (value | topic[j][index]),
+                    0
+                );
+
+            if (canBeSolved === maxNum) {
+                bestTeams++;
+            }
+
+            if (canBeSolved > maxNum) {
+                maxNum = canBeSolved;
+                bestTeams = 1;
+            }
+        }
+    }
+
+    return [maxNum, bestTeams];
+}
