54b6aaab32c2b74ba8296c4b0882b0999b26b121
cock45 <stardevworld@gmail.com>
2022-11-10 04:56:57 +0900

    Update: "#87 Use Sweeping Algorithm"

--- algorithm/beautiful-triplets.js
+++ algorithm/beautiful-triplets.js
@@ -1,28 +1,23 @@
 // O(n), n is length of array arr
 
 function beautifulTriplets(d, arr) {
-    const barr = new Array(d).fill(null).map(() => []);
-    const carr = new Array(d).fill(null).map(() => []);
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i - 1] !== arr[i]) {
-            barr[arr[i] % d].push(Number(arr[i]));
-            carr[arr[i] % d].push(1);
-        } else {
-            ++carr[arr[i] % d][barr[arr[i] % d].length - 1];
-        }
-    }
-
+    // Write your code here
     let count = 0;
+    let i = 0,
+        j = 0,
+        k = 0;
 
-    for (let i = 0; i < barr.length; i++) {
-        for (let j = 0; j < barr[i].length; j++) {
-            if (
-                barr[i][j] === barr[i][j - 1] + d &&
-                barr[i][j + 1] === barr[i][j] + d
-            ) {
-                count += carr[i][j - 1] * carr[i][j] * carr[i][j + 1];
-            }
+    for (i = 0; i < arr.length - 2; i++) {
+        while (arr[j] < arr[i] + d) {
+            j++;
+        }
+
+        while (arr[k] < arr[j] + d) {
+            k++;
+        }
+
+        if (arr[j] === arr[i] + d && arr[k] === arr[j] + d) {
+            count++;
         }
     }
 
