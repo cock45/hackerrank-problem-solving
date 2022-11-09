267da49c56a6d6a56a0e8955b9bab2bb6b06acc0
cock45 <stardevworld@gmail.com>
2022-11-09 23:01:21 +0900

    Update: "#87 Works in All Corner Cases"

--- algorithm/beautiful-triplets.js
+++ algorithm/beautiful-triplets.js
@@ -1,24 +1,27 @@
 // O(n), n is length of array arr
 
 function beautifulTriplets(d, arr) {
-    // Write your code here
-    const barr = arr.reduce(
-        (barr, value) => (barr[value % d].push(Number(value)), barr),
-        new Array(d).fill(null).map(() => [])
-    );
+    const barr = new Array(d).fill(null).map(() => []);
+    const carr = new Array(d).fill(null).map(() => []);
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i - 1] !== arr[i]) {
+            barr[arr[i] % d].push(Number(arr[i]));
+            carr[arr[i] % d].push(1);
+        } else {
+            ++carr[arr[i] % d][barr[arr[i] % d].length - 1];
+        }
+    }
 
     let count = 0;
-    let sameValueCount = 0;
 
-    for (let row of barr) {
-        for (let i = 1; i < row.length - 1; i++) {
-            if (row[i] === row[i - 1] + d && row[i + 1] === row[i] + d) {
-                count += 1 + sameValueCount;
-                sameValueCount = 0;
-            }
-
-            if (row[i - 1] === row[i]) {
-                sameValueCount++;
+    for (let i = 0; i < barr.length; i++) {
+        for (let j = 0; j < barr[i].length; j++) {
+            if (
+                barr[i][j] === barr[i][j - 1] + d &&
+                barr[i][j + 1] === barr[i][j] + d
+            ) {
+                count += carr[i][j - 1] * carr[i][j] * carr[i][j + 1];
             }
         }
     }
