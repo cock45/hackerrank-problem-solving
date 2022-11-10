ff56242c35f02bd519d11b89b991c5a824adb717
cock45 <stardevworld@gmail.com>
2022-11-11 05:23:26 +0900

    Feat: "Flatland Space Station"

---
+++ algorithm/flatland-space-stations.js
@@ -0,0 +1,18 @@
+// O(n), n is length of c
+
+function flatlandSpaceStations(n, c) {
+    c.sort((a, b) => a - b);
+    const space = [-c[0], ...c, 2 * (n - 1) - c.at(-1)];
+    console.log(space);
+
+    let max = 0;
+
+    for (let i = 1; i < space.length; i++) {
+        const dist = Math.floor((space[i] - space[i - 1]) / 2);
+        if (dist > max) {
+            max = dist;
+        }
+    }
+
+    return max;
+}
