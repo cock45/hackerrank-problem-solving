5fe36f3d9412a9badd6b45fbf2b94a091fb1c039
cock45 <stardevworld@gmail.com>
2022-11-11 05:49:27 +0900

    Update: "#90 Customize Code to be More Readable"

--- algorithm/the-time-in-words.js
+++ algorithm/the-time-in-words.js
@@ -1,50 +1,50 @@
 // O(1)
 
 function timeInWords(h, m) {
-    const numbers = [
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-        "ten",
-        "eleven",
-        "tweleve",
-        "thirteen",
-        "fourteen",
-        "quarter",
-        "sixteen",
-        "seventeen",
-        "eighteen",
-        "nineteen",
-        "twenty",
-        "twenty one",
-        "twenty two",
-        "twenty three",
-        "twenty four",
-        "twenty five",
-        "twenty six",
-        "twenty seven",
-        "twenty eight",
-        "twenty nine",
-        "half",
-    ];
-    const hour = m <= 30 ? h : (h + 1) % 12;
-    const min = m <= 30 ? m : 60 - m;
-    const prefix = m > 30 ? "to" : "past";
-    const unit = min === 1 ? "minute" : "minutes";
+    const numberToEng = (value) => {
+        const engNumbers = [
+            '',
+            'one',
+            'two',
+            'three',
+            'four',
+            'five',
+            'six',
+            'seven',
+            'eight',
+            'nine',
+            'ten',
+            'eleven',
+            'twelve',
+            'thirteen',
+            'fourteen',
+            'fifteen',
+            'sixteen',
+            'seventeen',
+            'eighteen',
+            'nineteen',
+            'twenty',
+        ];
+        return value <= 20
+            ? engNumbers[value]
+            : `twenty ${engNumbers[value - 20]}`;
+    };
 
-    if (min === 0) {
-        return [numbers[hour - 1], "o' clock"].join(" ");
+    if (m === 0) {
+        return `${numberToEng(h)} o' clock`;
+    } else if (m === 15) {
+        return `quarter past ${numberToEng(h)}`;
+    } else if (m === 30) {
+        return `half past ${numberToEng(h)}`;
+    } else if (m === 45) {
+        return `quarter to ${numberToEng(h + 1)}`;
+    } else if (m < 30) {
+        return `${numberToEng(m)} minute${m > 1 ? 's' : ''} past ${numberToEng(
+            h
+        )}`;
+    } else {
+        return `${numberToEng(60 - m)} minute${
+            m < 59 ? 's' : ''
+        } to ${numberToEng(h + 1)}`;
     }
-
-    if (min === 15 || min === 30) {
-        return [numbers[min - 1], prefix, numbers[hour - 1]].join(" ");
-    }
-
-    return [numbers[min - 1], unit, prefix, numbers[hour - 1]].join(" ");
 }
