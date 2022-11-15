e576de69ecba40ca0b699334006cf0226501eb89
cock45 <stardevworld@gmail.com>
2022-11-16 00:52:10 +0900

    Update: "Code Optimized"

--- algorithm/the-grid-search.js
+++ algorithm/the-grid-search.js
@@ -1,24 +1,12 @@
 // O(R * C * r * c), R, r and C, c, the number of rows in the search grid, pattern grid and the length of each row, pattern row string.
 
 function gridSearch(G, P) {
-    // Write your code here
-    const [gr, gc] = [G.length, G[0].length];
-    const [pr, pc] = [P.length, P[0].length];
+    const [R, C, r, c] = [G.length, G[0].length, C.length, C[0].length];
 
-    for (let r = 0; r <= gr - pr; r++) {
-        for (let c = 0; c < gc; c++) {
-            if (G[r].substr(c, pc) === P[0]) {
-                let machedRows = 0;
-
-                for (let p = 0; p < pr; p++) {
-                    if (G[r + p].substr(c, pc) === P[p]) {
-                        machedRows++;
-                    }
-                }
-
-                if (machedRows === pr) {
-                    return "YES";
-                }
+    for (let i = 0; i <= R - r; i++) {
+        for (let j = 0; j <= C - c; j++) {
+            if (P.every((row, k) => G[i + k].startsWith(row, j))) {
+                return "YES";
             }
         }
     }
