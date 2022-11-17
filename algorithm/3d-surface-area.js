d93847c3179b26b6e1178236a572262ecb8bac5f
cock45 <stardevworld@gmail.com>
2022-11-18 02:41:20 +0900

    Update: "#100 Optimized Code"

--- algorithm/3d-surface-area.js
+++ algorithm/3d-surface-area.js
@@ -1,31 +1,25 @@
-// O(H * W), H and W are numbers of Rows and Columns of A
+// O(HW), H and W are numbers of Rows and Columns of A
 
 function surfaceArea(A) {
-    // Write your code here
     const [H, W] = [A.length, A[0].length];
-    let [front, top, side] = [0, 0, 0];
 
-    top = H * W;
+    let result = 2 * H * W;
 
-    for (let row of A) {
-        for (let f = 0; f < W; f++) {
-            if (f === 0) {
-                front += row[f];
-            } else if (row[f] > row[f - 1]) {
-                front += row[f] - row[f - 1];
-            }
+    for (let i = 0; i < H; i++) {
+        result += A[i][0] + A[i][W - 1];
+
+        for (let j = 0; j < W - 1; j++) {
+            result += Math.abs(A[i][j] - A[i][j + 1]);
         }
     }
 
     for (let i = 0; i < W; i++) {
-        for (let j = 0; j < H; j++) {
-            if (j === 0) {
-                side += A[j][i];
-            } else if (A[j][i] > A[j - 1][i]) {
-                side += A[j][i] - A[j - 1][i];
-            }
+        result += A[0][i] + A[H - 1][i];
+
+        for (let j = 0; j < H - 1; j++) {
+            result += Math.abs(A[j][i] - A[j + 1][i]);
         }
     }
 
-    return 2 * (front + top + side);
+    return result;
 }
