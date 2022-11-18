d7f62ebe306f7a60eeff4e30dd4d7783faa9bd88
cock45 <stardevworld@gmail.com>
2022-11-19 01:26:55 +0900

    Update: "Final Optimized"

--- algorithm/sherlock-and-the-beast.js
+++ algorithm/sherlock-and-the-beast.js
@@ -4,9 +4,9 @@ function decentNumber(n) {
     const r = n % 3;
     const numOfThrees = r === 0 ? 0 : r === 1 ? 10 : 5;
 
-    if (n < 5 + 3 && n % 5 && n % 3) {
-        console.log(-1);
-    } else {
-        console.log("5".repeat(n - numOfThrees) + "3".repeat(numOfThrees));
-    }
+    console.log(
+        n < numOfThrees
+            ? -1
+            : "5".repeat(n - numOfThrees) + "3".repeat(numOfThrees)
+    );
 }
