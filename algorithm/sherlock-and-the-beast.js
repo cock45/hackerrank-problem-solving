778d3800d9dec6a631a994f370c343e41621aed9
cock45 <stardevworld@gmail.com>
2022-11-19 00:55:41 +0900

    Update: "Works Without variable d"

--- algorithm/sherlock-and-the-beast.js
+++ algorithm/sherlock-and-the-beast.js
@@ -1,15 +1,15 @@
 // Time Complexity is O(1)
 
 function decentNumber(n) {
-    const [d, r] = [Math.floor(n / 3), n % 3];
+    const r = n % 3;
 
     if (n < 5 + 3 && n % 5 && n % 3) {
         console.log(-1);
     } else if (r === 0) {
         console.log("5".repeat(n));
     } else if (r === 1) {
-        console.log("5".repeat(3 * (d - 3)) + "3".repeat(5 * 2));
+        console.log("5".repeat(n - 10) + "3".repeat(5 * 2));
     } else {
-        console.log("5".repeat(3 * (d - 1)) + "3".repeat(5));
+        console.log("5".repeat(n - 5) + "3".repeat(5));
     }
 }
