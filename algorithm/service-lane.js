db5fa4024a8265688c67e0fd3170f76c36c980c4
cock45 <stardevworld@gmail.com>
2022-11-22 02:34:19 +0900

    Update: "Using array.prototype.slice()"

--- algorithm/service-lane.js
+++ algorithm/service-lane.js
@@ -1,15 +1,7 @@
 // Time Complexity is O(nm|), n is length of width and m is length of cases.
 
 function serviceLane(n, width, cases) {
-    const result = cases.map((section) => {
-        let min = 3;
-        for (let i = section[0]; i <= section[1]; i++) {
-            if (width[i] < min) {
-                min = width[i];
-            }
-        }
-        return min;
-    });
-
-    return result;
+    return cases.map(([entry, exit]) =>
+        Math.min(...width.slice(entry, exit + 1))
+    );
 }
