b86944cda7d1149795537a0b7597d7ed572bfb52
cock45 <stardevworld@gmail.com>
2022-11-22 03:28:00 +0900

    Update: "Use Binanary Search Engine"

--- algorithm/intro-to-tutorial-challenges.js
+++ algorithm/intro-to-tutorial-challenges.js
@@ -1,5 +1,23 @@
-// Time Complexity is O(n)
+// Time Complexity is O(logn), n is length of arr
 
 function introTutorial(V, arr) {
-    return arr.indexOf(V);
+    function binarySearch(arr, x, low, high) {
+        if (low > high) {
+            return false;
+        }
+
+        const mid = Math.floor((low + high) / 2);
+
+        if (x === arr[mid]) {
+            return mid;
+        }
+
+        if (x > arr[mid]) {
+            return binarySearch(arr, x, mid + 1, high);
+        }
+
+        return binarySearch(arr, x, low, mid);
+    }
+
+    return binarySearch(arr, V, 0, arr.length - 1);
 }
