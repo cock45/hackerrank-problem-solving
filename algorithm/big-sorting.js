f8b395a89064fccedcd598a7d35dba219789f416
cock45 <stardevworld@gmail.com>
2022-11-22 04:00:25 +0900

    Update: "Use Logical AND"

--- algorithm/big-sorting.js
+++ algorithm/big-sorting.js
@@ -1,7 +1,7 @@
 // Time Complexity is O(nlogn)
 
 function bigSorting(unsorted) {
-    return unsorted.sort((a, b) =>
-        a.length !== b.length ? a.length - b.length : a < b ? -1 : 1
+    return unsorted.sort(
+        (a, b) => a.length - b.length || (a > b ? 1 : a < b ? -1 : 0)
     );
 }
