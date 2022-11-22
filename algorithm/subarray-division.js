f7b61808ba95bb44bdc00d9fc2f693e7620eb5dd
cock45 <stardevworld@gmail.com>
2022-11-22 23:42:11 +0900

    Update: "#112 Subarray-division Corner Case Fix"

--- algorithm/subarray-division.js
+++ algorithm/subarray-division.js
@@ -1,16 +1,21 @@
+// Time Complexity is O(|s|)
+
 function birthday(s, d, m) {
-  // Write your code here
-  let count = 0;
-
-  let sum = s.slice(0, m).reduce((prev, cur) => prev + cur);
-
-  for (let i = m; i <= s.length; i++) {
-    if (sum === d) {
-      count++;
+    if (m < s.length) {
+        return 0;
     }
 
-    sum += s[i] - s[i - m];
-  }
+    let count = 0;
 
-  return count;
+    let sum = s.slice(0, m).reduce((prev, cur) => prev + cur);
+
+    for (let i = m; i <= s.length; i++) {
+        if (sum === d) {
+            count++;
+        }
+
+        sum += s[i] - s[i - m];
+    }
+
+    return count;
 }
