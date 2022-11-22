0003a7843e7b954347f1dc6dd28d0e436d2592a3
cock45 <stardevworld@gmail.com>
2022-11-23 02:40:57 +0900

    Update: "Fix State Order Bug"

--- algorithm/subarray-division.js
+++ algorithm/subarray-division.js
@@ -8,12 +8,12 @@ function birthday(s, d, m) {
     let sum = s.slice(0, m).reduce((prev, cur) => prev + cur, 0);
     let count = sum === d ? 1 : 0;
 
-    for (let i = m; i <= s.length; i++) {
+    for (let i = m; i < s.length; i++) {
+        sum += s[i] - s[i - m];
+
         if (sum === d) {
             count++;
         }
-
-        sum += s[i] - s[i - m];
     }
 
     return count;
