d9147fb54304daaf9faad65254343d68e32f439b
cock45 <stardevworld@gmail.com>
2022-11-25 04:14:29 +0900

    Update: "Use Shift algorithm instead of Swap"

--- algorithm/insertion-sort-2.js
+++ algorithm/insertion-sort-2.js
@@ -2,17 +2,11 @@
 
 function insertionSort2(n, arr) {
     for (let i = 1; i < n; i++) {
-        for (let j = i - 1; j >= 0; j--) {
-            if (arr[j] <= arr[j + 1]) {
-                console.log(arr.join(" "));
-                break;
-            } else {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-            }
-            if (j === 0) {
-                console.log(arr.join(" "));
-                break;
-            }
+        const value = arr[i];
+        for (let j = i - 1; j >= 0 && arr[j] > value; j--) {
+            arr[j + 1] = arr[j];
+            arr[j] = value;
         }
+        console.log(arr.join(" "));
     }
 }
