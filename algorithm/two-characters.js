8d1b16c7b6f5e7f902254312964318b2a32b616c
cock45 <stardevworld@gmail.com>
2022-11-25 04:38:26 +0900

    Update: "Defined and Used isAlternation()"

--- algorithm/two-characters.js
+++ algorithm/two-characters.js
@@ -1,27 +1,19 @@
 // Time Complexity is O(|s|)
 
 function alternate(s) {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz";
-    let max = 0;
-    const sArr = Array.from(s);
+    let maxLen = 0;
+    const codes = Array.from(s).map((letter) => letter.charCodeAt(0) - 0x61);
+    const isAlternating = (t) =>
+        !(t < 2 || t.slice(1).some((v, i) => v === t[i]));
 
     for (let i = 0; i < 26; i++) {
         for (let j = i + 1; j < 26; j++) {
-            const subArr = sArr.filter(
-                (letter, index) =>
-                    letter === alphabet[i] || letter === alphabet[j]
-            );
-            let subLength = subArr.length;
-            for (let k = 1; k < subLength; k++) {
-                if (subArr[k - 1] === subArr[k]) {
-                    subLength = 0;
-                }
-            }
-            if (subLength > max) {
-                max = subLength;
+            const t = codes.filter((code) => code === i || code === j);
+            if (isAlternating(t) && t.length > maxLen) {
+                maxLen = t.length;
             }
         }
     }
 
-    return s.length === 1 ? 0 : max;
+    return maxLen;
 }
