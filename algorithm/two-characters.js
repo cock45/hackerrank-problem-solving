a9ba9789bec7ed9ca09e193ca7c7c3d00553001a
cock45 <stardevworld@gmail.com>
2022-11-25 05:02:42 +0900

    Update: "More Readable"

--- algorithm/two-characters.js
+++ algorithm/two-characters.js
@@ -1,11 +1,12 @@
 // Time Complexity is O(|s|)
 
 function alternate(s) {
-    let maxLen = 0;
-    const codes = Array.from(s).map((letter) => letter.charCodeAt(0) - 0x61);
     const isAlternating = (t) =>
         !(t < 2 || t.slice(1).some((v, i) => v === t[i]));
 
+    const codes = Array.from(s).map((letter) => letter.charCodeAt(0) - 0x61);
+
+    let maxLen = 0;
     for (let i = 0; i < 26; i++) {
         for (let j = i + 1; j < 26; j++) {
             const t = codes.filter((code) => code === i || code === j);
