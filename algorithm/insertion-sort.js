7b30e4b6dc6e30b74aec4edc1d82caebc9bb24ea
cock45 <stardevworld@gmail.com>
2022-11-24 09:38:54 +0900

    Update: "Remove n=1 case exception"

--- algorithm/insertion-sort.js
+++ algorithm/insertion-sort.js
@@ -1,14 +1,10 @@
 // O(n)
 
 function insertionSort1(n, arr) {
-    if (n === 1) {
-        console.log(arr.join(""));
-    }
-
     const rightmost = arr[n - 1];
-    let i = 0;
+    let i;
 
-    for (i = n - 2; i > 0, arr[i] > rightmost; i--) {
+    for (i = n - 2; i >= 0 && arr[i] > rightmost; i--) {
         arr[i + 1] = arr[i];
         console.log(arr.join(" "));
     }
