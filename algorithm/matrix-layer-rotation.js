fa17fcd0dbf4029a64425e4e592534830aaa6ba1
cock45 <stardevworld@gmail.com>
2022-11-26 05:34:21 +0900

    Update: "Change Variable Names as more Readable"

--- algorithm/matrix-layer-rotation.js
+++ algorithm/matrix-layer-rotation.js
@@ -8,26 +8,26 @@ function matrixRotation(matrix, r) {
     for (let layer = 0; 2 * layer < row && 2 * layer < col; layer++) {
         const circle = [];
 
-        for (let i = layer; i < row - layer - 1; i++) {
-            circle.push([i, layer]);
+        for (let y = layer; y < row - layer - 1; y++) {
+            circle.push([y, layer]);
         }
 
-        for (let i = layer; i < col - layer - 1; i++) {
-            circle.push([row - layer - 1, i]);
+        for (let x = layer; x < col - layer - 1; x++) {
+            circle.push([row - layer - 1, x]);
         }
 
-        for (let i = row - layer - 1; i > layer; i--) {
-            circle.push([i, col - layer - 1]);
+        for (let y = row - layer - 1; y > layer; y--) {
+            circle.push([y, col - layer - 1]);
         }
 
-        for (let i = col - layer - 1; i > layer; i--) {
-            circle.push([layer, i]);
+        for (let x = col - layer - 1; x > layer; x--) {
+            circle.push([layer, x]);
         }
 
         for (let k = 0; k < circle.length; k++) {
-            const [i, j] = circle[k];
-            const [ni, nj] = circle[(r + k) % circle.length];
-            result[ni][nj] = matrix[i][j];
+            const [y, x] = circle[k];
+            const [ny, nx] = circle[(r + k) % circle.length];
+            result[ny][nx] = matrix[y][x];
         }
     }
 
