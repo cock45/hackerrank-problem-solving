6e046bb4adcb01c5daf7071866d18a4dc262c34c
cock45 <stardevworld@gmail.com>
2022-11-29 06:44:16 +0900

    Update: "Optimized by using Array.flat(), fill()"

--- algorithm/counting-sort-2.js
+++ algorithm/counting-sort-2.js
@@ -2,18 +2,8 @@
 // Here n is length of arr
 
 function countingSort(arr) {
-    const frequences = arr.reduce(
-        (freq, value) => (++freq[value], freq),
-        new Array(100).fill(0)
-    );
-
-    const result = [];
-
-    for (let i = 0; i < frequences.length; i++) {
-        for (let f = 0; f < frequences[i]; f++) {
-            result.push(i);
-        }
-    }
-
-    return result;
+    return arr
+        .reduce((freq, value) => (++freq[value], freq), new Array(100).fill(0))
+        .map((value, index) => new Array(value).fill(index))
+        .flat();
 }
