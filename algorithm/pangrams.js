01b006b50abcf1974f86687f95f88d74c48955a0
cock45 <stardevworld@gmail.com>
2022-11-29 00:35:11 +0900

    Update: "Used new Set()"

--- algorithm/pangrams.js
+++ algorithm/pangrams.js
@@ -1,23 +1,11 @@
 // Time Complexity is O(|s|)
 
 function pangrams(s) {
-    const targetCodes = new Array(26).fill(0);
-
-    for (const letter of s) {
-        const code = letter.charCodeAt(0);
-
-        if (code >= 0x41 && code <= 0x5a) {
-            targetCodes[code - 0x41] = 1;
-        }
-
-        if (code >= 0x61 && code <= 0x7a) {
-            targetCodes[code - 0x61] = 1;
-        }
-    }
-
-    const includeLetters = targetCodes.filter(
-        (isInclude) => isInclude > 0
-    ).length;
-
-    return includeLetters === 26 ? "pangram" : "not pangram";
+    return new Set(
+        Array.from(s.toLowerCase()).filter(
+            (letter) => letter >= "a" && letter <= "z"
+        )
+    ).size === 26
+        ? "pangram"
+        : "not pangram";
 }
