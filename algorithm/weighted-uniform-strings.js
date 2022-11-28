e39f844817a921e802acb5ca11796c58b54eb72d
cock45 <stardevworld@gmail.com>
2022-11-29 02:46:42 +0900

    Update: "Optimized as Work in O(n + |s|)"

--- algorithm/weighted-uniform-strings.js
+++ algorithm/weighted-uniform-strings.js
@@ -1,15 +1,30 @@
-// Time Complexity is O(n|s|)
+// Time Complexity is O(n + |s|)
 // Here n is length of queries
 
 function weightedUniformStrings(s, queries) {
-    const weights = new Array(s.length).fill(0);
-    let curWeight = 0;
+    const codes = Array.from(s).map((letter) => letter.charCodeAt(0) - 0x60);
 
-    for (let i = 0; i < s.length; i++) {
-        const weight = s.charCodeAt(i) - 0x60;
-        curWeight = i !== 0 && s[i] === s[i - 1] ? curWeight + weight : weight;
-        weights[i] = curWeight;
+    const freq = new Array(27).fill(0);
+
+    let [head, tail] = [0, 0];
+
+    while (tail < s.length) {
+        if (s[tail] !== s[head]) {
+            head = tail;
+        }
+
+        if (freq[codes[tail]] < tail - head + 1) {
+            freq[codes[tail]] = tail - head + 1;
+        }
+
+        tail++;
     }
 
-    return queries.map((query) => (weights.indexOf(query) > -1 ? "Yes" : "No"));
+    return queries.map((value) => {
+        return freq.some(
+            (count, weight) => value % weight === 0 && value <= count * weight
+        )
+            ? "Yes"
+            : "No";
+    });
 }
