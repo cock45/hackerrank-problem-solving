346ec4d9eab6b06f5f571ecd9492b4722ed559a5
cock45 <stardevworld@gmail.com>
2022-11-29 00:17:24 +0900

    Update: "Not Sorted As Requirement"

--- algorithm/quickSort-1-partition.js
+++ algorithm/quickSort-1-partition.js
@@ -2,12 +2,15 @@
 // Here n is length of arr
 
 function quickSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        const value = arr[i];
+    const pivot = arr[0];
 
-        for (let j = i - 1; j >= 0 && arr[j] > value; j--) {
-            arr[j + 1] = arr[j];
-            arr[j] = value;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            const value = arr[i];
+            for (let j = i - 1; j >= 0 && arr[j] >= pivot; j--) {
+                arr[j + 1] = arr[j];
+                arr[j] = value;
+            }
         }
     }
 
