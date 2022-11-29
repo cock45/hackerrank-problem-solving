46f91d5517233a8dadbf4f1995d57b2f1500f653
cock45 <stardevworld@gmail.com>
2022-11-30 01:02:01 +0900

    Update "Work with one variable instead of two"

--- algorithm/making-anagrams.js
+++ algorithm/making-anagrams.js
@@ -1,15 +1,14 @@
 // Time Complexity is O(|s1| + |s2|)
 
 function makingAnagrams(s1, s2) {
-    const freq1 = Array.from(s1).reduce(
-        (freq, letter) => (++freq[letter.charCodeAt(0) - 0x61], freq),
-        new Array(26).fill(0)
-    );
+    const freq = [s1, s2]
+        .map((s) =>
+            Array.from(s).reduce(
+                (f, v, i) => (f[s.charCodeAt(i) - 0x61]++, f),
+                new Array(26).fill(0)
+            )
+        )
+        .flat();
 
-    const freq2 = Array.from(s2).reduce(
-        (freq1, letter) => (--freq1[letter.charCodeAt(0) - 0x61], freq1),
-        freq1
-    );
-
-    return freq2.reduce((sum, cur) => sum + Math.abs(cur), 0);
+    return freq.slice(26).reduce((acc, v, i) => acc + Math.abs(v - freq[i]), 0);
 }
