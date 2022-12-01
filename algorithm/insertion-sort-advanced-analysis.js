ed2dcffe153062d5e2e6f8fb7ff1a57460d4d03e
cock45 <stardevworld@gmail.com>
2022-12-02 06:25:43 +0900

    Update: "#143 Modified Function Names"

--- algorithm/insertion-sort-advanced-analysis.js
+++ algorithm/insertion-sort-advanced-analysis.js
@@ -4,20 +4,20 @@
 
 function insertionSort(arr) {
     const N = Math.max(...arr);
-    const BITree = new Array(N + 1).fill(0);
+    const c = new Array(N + 1).fill(0);
 
-    function getSum(index) {
+    function sum(index) {
         let sum = 0;
         while (index > 0) {
-            sum += BITree[index];
+            sum += c[index];
             index -= index & -index;
         }
         return sum;
     }
 
-    function updateBIT(n, index, value) {
+    function update(n, index, value) {
         while (index <= n) {
-            BITree[index] += value;
+            c[index] += value;
             index += index & -index;
         }
     }
@@ -25,8 +25,8 @@ function insertionSort(arr) {
     const M = arr.length;
     let result = (M * (M - 1)) / 2;
     for (let i = 0; i < M; i++) {
-        result -= getSum(arr[i]);
-        updateBIT(N, arr[i], 1);
+        result -= sum(arr[i]);
+        update(N, arr[i], 1);
     }
 
     return result;
