0169f8e329d896dbef1531dcfa86c4c8d6dc29c8
cock45 <stardevworld@gmail.com>
2022-12-03 11:58:31 +0900

    Resolve "#160 Array Manipulation"

---
+++ data-structure/array-manipulation.js
@@ -0,0 +1,18 @@
+// Time Complexity is O(n + m)
+// Here n is length of array
+// And m is length of queries
+
+function arrayManipulation(n, queries) {
+    const arr = new Array(n + 1).fill(0);
+
+    for (const [a, b, k] of queries) {
+        arr[a] += k;
+        arr[b + 1] -= k;
+    }
+
+    for (let i = 1; i < n; i++) {
+        arr[i + 1] += arr[i];
+    }
+
+    return arr.reduce((max, cur) => (cur > max ? cur : max), 0);
+}
