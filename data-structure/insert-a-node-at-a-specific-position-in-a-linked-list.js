cc00dc91c0e48fc624f30c87db197fb06ae29363
cock45 <stardevworld@gmail.com>
2022-12-05 06:59:49 +0900

    Resolve: "#161 Insert Node At Position LinkedList"

---
+++ data-structure/insert-a-node-at-a-specific-position-in-a-linked-list.js
@@ -0,0 +1,27 @@
+// Time Complexity is O(n)
+// Here n is position
+
+function insertNodeAtPosition(llist, data, position) {
+    const newNode = new SinglyLinkedListNode(data);
+
+    if (position === 0) {
+        newNode.next === llist;
+        return newNode;
+    }
+
+    let curNode = llist;
+
+    for (let i = 1; i < position; i++) {
+        curNode = curNode.next;
+    }
+
+    if (curNode.next === null) {
+        curNode.next = newNode;
+    } else {
+        const temp = curNode.next;
+        curNode.next = newNode;
+        newNode.next = temp;
+    }
+
+    return llist;
+}
