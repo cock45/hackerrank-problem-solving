0b20fc8a4fc9f434ef7aeaef91547673c1e775cc
Dante-top <dantetopdev@gmail.com>
2022-12-07 10:55:53 +0900

    Update "#169 Styled using Formatter"

--- data-structure/cycle-detection.cpp
+++ data-structure/cycle-detection.cpp
@@ -1,15 +1,19 @@
 // Time Complexity is O(n)
 // Here n is size of llist / 2
 
-bool has_cycle(SinglyLinkedListNode* head) {
-    SinglyLinkedListNode* slow = head;
-    SinglyLinkedListNode* fast = head;
-    while (slow && fast && fast -> next) {
+bool has_cycle(SinglyLinkedListNode *head)
+{
+    SinglyLinkedListNode *slow = head;
+    SinglyLinkedListNode *fast = head;
+    while (slow && fast && fast->next)
+    {
         slow = slow->next;
         fast = fast->next->next;
-        if (slow == fast) {
+        if (slow == fast)
+        {
             return 1;
         }
     }
+
     return 0;
 }
