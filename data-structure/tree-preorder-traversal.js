6ce9817e72e52b393c9c683ce9dc8e3037dc1ace
Dante-top <dantetopdev@gmail.com>
2022-12-09 02:03:51 +0900

    Update "Used Generate Function"

--- data-structure/tree-preorder-traversal.js
+++ data-structure/tree-preorder-traversal.js
@@ -2,9 +2,14 @@
 // Here n is number of Nodes in the tree
 
 function preOrder(root) {
-    if (root) {
-        process.stdout.write(root.data + " ");
-        preOrder(root.left);
-        preOrder(root.right);
+    function* traversal(root) {
+        if (!root) {
+            return;
+        }
+
+        yield root.data;
+        yield* traversal(root.left);
+        yield* traversal(root.right);
     }
+    console.log([...traversal(root)].join(" "));
 }
