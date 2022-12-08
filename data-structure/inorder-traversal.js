ab83e2e08762608db132405f8238a2b34a2dcf43
Dante-top <dantetopdev@gmail.com>
2022-12-09 02:08:27 +0900

    Update "Used Generate Function"

--- data-structure/inorder-traversal.js
+++ data-structure/inorder-traversal.js
@@ -2,9 +2,14 @@
 // Here n is number of Nodes in Tree
 
 function inOrder(root) {
-    if (root) {
-        inOrder(root.left);
-        process.stdout.write(root.data + " ");
-        inOrder(root.right);
+    function* traversal(root) {
+        if (!root) {
+            return;
+        }
+
+        yield* traversal(root.left);
+        yield root.data;
+        yield* traversal(root.right);
     }
+    console.log([...traversal(root)].join(" "));
 }
