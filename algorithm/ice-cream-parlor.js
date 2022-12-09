8e459740baf82c64089e1ad01ee63867c96c3f70
Dante-top <dantetopdev@gmail.com>
2022-12-09 23:44:02 +0900

    Update "More Readable"

--- algorithm/ice-cream-parlor.js
+++ algorithm/ice-cream-parlor.js
@@ -2,21 +2,24 @@
 // Here n is length of arr
 
 function icecreamParlor(m, arr) {
-    const indexedArr = arr.map((v, i) => [v, i + 1]).sort(([a], [b]) => a - b);
+    const sorted = arr
+        .map((value, key) => ({ key, value }))
+        .sort((a, b) => a.value - b.value);
 
     let head = 0,
         tail = arr.length - 1;
 
-    while (head < tail) {
-        const a = indexedArr[head];
-        const b = indexedArr[tail];
-
-        if (a[0] + b[0] === m) {
-            return [a[1], b[1]].sort((a, b) => a - b);
-        } else if (a[0] + b[0] > m) {
+    while (true) {
+        while (sorted[head].value + sorted[tail].value > m) {
             tail--;
-        } else {
-            head++;
         }
+
+        if (sorted[head].value + sorted[tail].value === m) {
+            return [sorted[head].key + 1, sorted[tail].key + 1].sort(
+                (a, b) => a - b
+            );
+        }
+
+        head++;
     }
 }
