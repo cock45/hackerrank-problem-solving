5c5298249d99b912483e55385f6335a540a4b28a
Dante-top <dantetopdev@gmail.com>
2022-12-09 22:39:39 +0900

    Update "Code Optimized using Array.some()"

--- algorithm/two-strings.js
+++ algorithm/two-strings.js
@@ -1,26 +1,12 @@
 // Time Complexity is O(|s1| + |s2|)
 
 function twoStrings(s1, s2) {
-    if (s1.length === 1 || s2.length === 1) {
-        return 0;
-    }
+    const freq = Array.from(s1).reduce(
+        (f, letter) => (++f[letter.charCodeAt(0) - 0x61], f),
+        new Array(26).fill(0)
+    );
 
-    function getFreq(str) {
-        const freq = new Array(26).fill(0);
-        for (const letter of str) {
-            ++freq[letter.charCodeAt(0) - 0x61];
-        }
-        return freq;
-    }
-
-    const freq1 = getFreq(s1);
-    const freq2 = getFreq(s2);
-
-    for (let i = 0; i < 26; i++) {
-        if (freq1[i] && freq2[i]) {
-            return "YES";
-        }
-    }
-
-    return "NO";
+    return Array.from(s2).some((letter) => freq[letter.charCodeAt(0) - 0x61])
+        ? "YES"
+        : "NO";
 }
