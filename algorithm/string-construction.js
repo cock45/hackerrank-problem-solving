0c7726f453d1b1d557239b4e3d71586df6a86f9b
Dante-top <dantetopdev@gmail.com>
2022-12-09 22:44:42 +0900

    Update "Used new Set().size"

--- algorithm/string-construction.js
+++ algorithm/string-construction.js
@@ -1,11 +1,5 @@
 // Time Complexity is O(|s|)
 
 function stringConstruction(s) {
-    const freq = new Array(26).fill(0);
-
-    for (const letter of s) {
-        freq[letter.charCodeAt(0) - 0x61] = 1;
-    }
-
-    return freq.filter((v) => v > 0).length;
+    return new Set(s).size;
 }
