f9f80505caa89dbc9631bb5cd505370f677a13ea
cock45 <stardevworld@gmail.com>
2022-12-12 05:02:12 +0900

    Update "Not defined sorted variable"

--- algorithm/luck-balance.js
+++ algorithm/luck-balance.js
@@ -1,13 +1,13 @@
 function luckBalance(k, contests) {
     let count = 0,
         sum = 0;
-    const sorted = [...contests].sort(([a], [b]) => b - a);
+    contests.sort(([a], [b]) => b - a);
 
-    for (const [L, T] of sorted) {
-        if (T === 1) {
+    for (const [luck, importance] of contests) {
+        if (importance === 1) {
             count++;
         }
-        sum += count > k && T === 1 ? -L : L;
+        sum += count > k && importance === 1 ? -luck : luck;
     }
 
     return sum;
