20f7e85d660c0696a90dbc763988e307786ef327
cock45 <stardevworld@gmail.com>
2022-12-13 06:05:28 +0900

    Update "More Readable"

--- algorithm/Maximum Perimeter Triangle/main.js
+++ algorithm/Maximum Perimeter Triangle/main.js
@@ -1,10 +1,11 @@
 // O(n)
 
 function maximumPerimeterTriangle(sticks) {
-    sticks.sort((a, b) => a - b);
-    for (let i = sticks.length - 1; i >= 2; i--) {
-        if (sticks[i] < sticks[i - 1] + sticks[i - 2]) {
-            return [sticks[i - 2], sticks[i - 1], sticks[i]];
+    sticks.sort((a, b) => b - a);
+    for (let i = 0; i < sticks.length - 2; i++) {
+        const [a, b, c] = [sticks[i], sticks[i + 1], sticks[i + 2]];
+        if (a < b + c) {
+            return [c, b, a];
         }
     }
 
