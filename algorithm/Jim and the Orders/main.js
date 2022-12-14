d8cda23bd9f4398c0a206cd552f06a6d4d0826e6
cock45 <stardevworld@gmail.com>
2022-12-14 12:08:04 +0900

    Update "Check the customer index sort"

--- algorithm/Jim and the Orders/main.js
+++ algorithm/Jim and the Orders/main.js
@@ -1,10 +1,11 @@
 // O(nlogn)
 
 function jimOrders(orders) {
-    const serveTime = orders.map(([order, prep], index) => [
-        order + prep,
-        index + 1,
-    ]);
-
-    return serveTime.sort(([a], [b]) => a - b).map(([time, index]) => index);
+    const serveTime = orders
+        .map(([order, prepTime], customer) => ({
+            time: order + prepTime,
+            customer: customer + 1,
+        }))
+        .sort((a, b) => a.time - b.time || a.customer - b.customer)
+        .map(({ customer }) => customer);
 }
