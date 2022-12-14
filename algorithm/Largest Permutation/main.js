99aee08fbc439bb0036f4963b2f275ef7bac8a2a
cock45 <stardevworld@gmail.com>
2022-12-14 10:53:07 +0900

    Update "Used n as dynamic variable"

--- algorithm/Largest Permutation/main.js
+++ algorithm/Largest Permutation/main.js
@@ -1,22 +1,25 @@
 // O(n)
 
 function largestPermutation(k, arr) {
-    const n = arr.length;
+    let n = arr.length;
 
-    const indices = new Array(n + 1).fill(0);
+    const order = new Array(n + 1).fill(0);
     for (let i = 0; i < n; i++) {
-        indices[arr[i]] = i;
+        order[arr[i]] = i;
     }
 
-    for (let i = 0; i < k && i < n; i++) {
-        if (arr[i] === n - i) {
-            k++;
+    while (n && k) {
+        const [i, j] = [arr.length - n, order[n--]];
+
+        if (i === j) {
             continue;
         }
-        const moveFrom = indices[n - i];
-        const numberToExchange = arr[i];
-        [arr[i], arr[moveFrom]] = [n - i, arr[i]];
-        indices[numberToExchange] = moveFrom;
+
+        order[arr[i]] = j;
+
+        [arr[i], arr[j]] = [[arr[j]], [arr[i]]];
+
+        k--;
     }
 
     return arr;
